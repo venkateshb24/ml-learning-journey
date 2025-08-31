c8d20ce1283c5f30de251eb1d02149eead97bc1f
venkateshb24 <venkatesh.b2024aiml@sece.ac.in>
2025-08-31 19:36:16 +0530

    Copy vs View

--- numpy-practice.py
+++ numpy-practice.py
@@ -1,40 +1,17 @@
 import numpy as np
 
-# universal functions (ufuncs) are functions that operate element-wise on ndarrays
+# copy vs view
+arr = np.array([1, 2, 3, 4, 5])
+arr_copy = arr.copy()
+arr_view = arr.view()
 
-arr = np.array([1, 2, 3, 4])
+arr[0] = 10
 print("Original array:", arr)
+print("Copy of array:", arr_copy)
+print("View of array:", arr_view)
 
-# Example of a ufunc: np.sqrt
-sqrt_arr = np.sqrt(arr)
-print("Square root of each element:", sqrt_arr)
-
-np2 = np.array([-3, -2, -1, 0, 1, 2, 3])
-# absolute value
-abs_arr = np.abs(np2)
-print("Absolute values:", abs_arr)
-
-# exponential
-exp_arr = np.exp(arr)
-print("Exponential of each element:", exp_arr)
-
-# min and max
-min_val = np.min(arr)
-max_val = np.max(arr)
-print("Min value:", min_val)
-print("Max value:", max_val)
-
-# sign positive, negative, or zero
-sign_arr = np.sign(np2)
-print("Sign of each element:", sign_arr)    
-
-# trig sin cos log
-sin_arr = np.sin(arr)
-cos_arr = np.cos(arr)
-log_arr = np.log(arr)
-print("Natural logarithm of each element:", log_arr)
-print("Sine of each element:", sin_arr)
-print("Cosine of each element:", cos_arr)
-
-# and much more...
-
+# change in view reflects in original
+arr_view[1] = 20
+print("After modifying view:")
+print("Original array:", arr)
+print("View of array:", arr_view)
